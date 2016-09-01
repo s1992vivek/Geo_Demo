d964f956c48da5c30e30fe6a5fa3c25adbbc22c8
s1992vivek <s1992vivek@gmail.com>
2016-09-01 18:58:16 +0530

    working

--- dataextract.py
+++ dataextract.py
@@ -1,8 +1,7 @@
 import random
 import csv
 City={}
-included_cols=[1]
-j=1
+j=0
 with open(r'/home/delhivery/Downloads/Cities/Delhi.csv', 'rb') as f:
     next(f, None)
     reader=csv.reader(f)
@@ -14,44 +13,45 @@ print len(City)
 print City[2800][0], City[2800][1]
 D={}
 
-for g in range(1,len(City)+1):
+for g in range(len(City)):
 	
 	lat_c=City[g][0]
 	long_c=City[g][1]
 	F=[]
 	for i in range(41):
-		lat=lat_c-(i-20)*.001
+		lat=lat_c-(i-21)*.001
 		for j in range(41):
-			lon=long_c-(j-20)*.001
+			lon=long_c-(j-21)*.001
 			tup=(round(lat,3),round(lon,3))
 			F.append(tup)       
 	F1=[F,1681]
 	D[g]=F1
 checked=[]
 total=len(City)*1681
-print D[2400][0][1500][0]
+print D[1222][0][1680][0]
 len_city=len(City)
 '''apply geocode'''
 
 while (total>0):
-	loc_rand= random.randint(1,len_city)
+	loc_rand= random.randint(0,len_city-1)
 	if len(checked)==len_city:
 		break
 	if loc_rand in checked:
 		continue
 
 	if D[loc_rand][1]>1:
-		grid_rand=random.randint(1,D[loc_rand][1])
+		grid_rand=random.randint(0,D[loc_rand][1]-1)
 	elif D[loc_rand][1]==1:
-		grid_rand=1
+		grid_rand=0
 
 	else:
 		checked.append(loc_rand)
 		del D[loc_rand]
-		print 'qwertyuiopasdfghjklxcvbnm,xcvbnm,fghjk'
+		print 'qwertyuiopasdfghjklxcvbnm,xcvbnm,fghjk', len(checked)
 		continue
 	# pt=D[loc_rand][0][grid_rand]
 	'apply geocode on rand_lat rand_long'
+	print loc_rand,grid_rand
 	rand_lat=D[loc_rand][0][grid_rand][0]
 	rand_long=D[loc_rand][0][grid_rand][1]
 
